3d109b9eeca9c56403bddd4b7b01ee85d9454798
Munjie <31505257+Munjie@users.noreply.github.com>
2018-01-17 09:56:50 +0800

    Update MyInterceptor.java

--- src/main/java/com/mwj/action/MyInterceptor.java
+++ src/main/java/com/mwj/action/MyInterceptor.java
@@ -7,13 +7,13 @@ public class MyInterceptor extends AbstractInterceptor {
     public String intercept(ActionInvocation actionInvocation) throws Exception {
 
 
-        System.out.println("begin");
+        System.out.println("begintime");
         long l = System.currentTimeMillis();
 
         String invoke = actionInvocation.invoke();
         long l1 = System.currentTimeMillis();
         long l2 = l-l1;
-        System.out.println("end"+l2);
+        System.out.println("endtime"+l2);
         return  invoke;
     }
 }
